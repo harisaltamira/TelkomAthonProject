1ef0ff14e117325ad31a35af3dae4ffee4b764f4
Haris Altamira <harisaltamira@gmail.com>
2021-07-23 10:40:33 +0700

    test 2

---
+++ App copy.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+import {
+  Text,
+  TextInput,
+  View,
+  SafeAreaView,
+  ScrollView,
+  FlatList,
+  StyleSheet,
+  Image,
+  ImageBackground,
+  TouchableOpacity,
+} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Login from './Source/Screen/Login/Login';
+import BottomTabsNavigation from './Source/Screen/BottomTabsNavigatior/BottomTabsNavigation';
+import Register from './Source/Screen/Register/Register';
+import Home from './Source/Screen/Home/Home';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen
+          name="BottomTabsNavigation"
+          component={BottomTabsNavigation}
+        />
+        <Stack.Screen name="Register" component={Register} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
