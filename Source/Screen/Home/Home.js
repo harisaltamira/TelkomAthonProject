04b81d7c1666f0f2bd74f6c287bb930aa7b98470
Haris Altamira <harisaltamira@gmail.com>
2021-08-24 16:00:48 +0700

    update: 24.08.2021

--- Source/Screen/Home/Home.js
+++ Source/Screen/Home/Home.js
@@ -10,8 +10,10 @@ import {
   Image,
   ImageBackground,
   TouchableOpacity,
+  Platform,
 } from 'react-native';
 import Modal from 'react-native-modal';
+import {NotificationService} from '../Service/Notification';
 
 class Home extends Component {
   constructor(props) {
@@ -20,6 +22,17 @@ class Home extends Component {
     const {params} = this.props.route;
     const name = params ? params.data : null;
     this.state.username = name;
+  }
+
+  notificationTrigger() {
+    if (Platform.OS == 'ios') {
+      //notification for ios
+      NotificationService.onSendNotificationIos();
+    } else {
+      NotificationService.configure();
+      NotificationService.createChannel('1');
+      NotificationService.sendNotification('1', 'test', 'test works');
+    }
   }
 
   render() {
@@ -31,6 +44,19 @@ class Home extends Component {
           onPress={() => this.props.navigation.navigate('Login')}
           style={styles.backToLoginButton}>
           <Text style={styles.backToLoginButtonText}>Logout</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => this.notificationTrigger()}
+          style={{
+            width: 180,
+            height: 50,
+            marginVertical: 10,
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: 10,
+            backgroundColor: 'pink',
+          }}>
+          <Text>Local Notification</Text>
         </TouchableOpacity>
       </SafeAreaView>
     );
